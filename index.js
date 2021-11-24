0aadb94ac0db1d656d491be8c2850c64a92fc560
Felix Sommer <felixissommer@hotmail.com>
2021-11-24 21:43:29 +0100

    integrate db

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const useragent = require('express-useragent');
-const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
+const ObjectId = require('mongodb').ObjectId;
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const client = new MongoClient(process.env.DB_URL);
+
 const port = process.env.PORT || "8080";
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
-
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
-    var source = req.headers['user-agent']
-    var ua = useragent.parse(source);
-    var { appleUrl, androidUrl, windowsUrl, elseUrl } = req.query
-    console.log(appleUrl, androidUrl, windowsUrl, elseUrl)
-    console.log(ua)
-    let redirectUrl = ''
-    if (ua.isWindows) {
-        // redirectUrl = 'https://microsoft.com'
-        redirectUrl = windowsUrl ?? elseUrl ?? 'https://microsoft.com'
-    } else if (isApple(ua)) {
-        // redirectUrl = 'https://apple.com'
-        redirectUrl = appleUrl ?? elseUrl ?? 'https://apple.com'
-    } else if (isAndroid(ua)) {
-        // redirectUrl = 'https://google.com'
-        redirectUrl = androidUrl ?? elseUrl ?? 'https://google.com'
-    } else {
-        redirectUrl = elseUrl ?? windowsUrl ?? androidUrl ?? appleUrl ?? 'https://microsoft.com'
-    }
-    //res.status(200).send(`WHATABYTE: Food For Devs ${ua}`, );
+    const redirectUrl = redirect(req)
+    res.redirect(301, redirectUrl)
+});
+
+app.post("/link", async (req, res) => {
+    const linkId = await insertLink(req.body)
+    const returnUrl = process.env.RETURN_URL + linkId
+    res.send({returnUrl})
+});
+
+app.get("/:linkId", async (req, res) => {
+    const linkData = await findLink(req.params.linkId)
+    const redirectUrl = redirectDB(req, linkData)
     res.redirect(301, redirectUrl)
 });
 
@@ -38,4 +37,55 @@ function isApple(ua) {
 
 function isAndroid(ua) {
     return ua.isAndroid || ua.isAndroidNative || ua.isChromeOS
+}
+
+
+async function insertLink(insertDoc) {
+    await client.connect();
+    const db = client.db(process.env.DB_NAME)
+    const collection = db.collection(process.env.DB_COLLECTION)
+    const insertResult = await collection.insertOne(insertDoc)
+    return insertResult.insertedId.toString()
+}
+
+async function findLink(linkId) {
+    await client.connect();
+    const db = client.db(process.env.DB_NAME)
+    const collection = db.collection(process.env.DB_COLLECTION)
+    const result = await collection.findOne({ "_id": ObjectId(linkId) })
+    return result
+}
+
+function redirect(req) {
+    var source = req.headers['user-agent']
+    var ua = useragent.parse(source);
+    var { appleUrl, androidUrl, windowsUrl, elseUrl } = req.query
+    let redirectUrl = ''
+    if (ua.isWindows) {
+        redirectUrl = windowsUrl ?? elseUrl ?? 'https://microsoft.com'
+    } else if (isApple(ua)) {
+        redirectUrl = appleUrl ?? elseUrl ?? 'https://apple.com'
+    } else if (isAndroid(ua)) {
+        redirectUrl = androidUrl ?? elseUrl ?? 'https://google.com'
+    } else {
+        redirectUrl = elseUrl ?? windowsUrl ?? androidUrl ?? appleUrl ?? 'https://microsoft.com'
+    }
+    return redirectUrl
+}
+
+function redirectDB(req, linkData) {
+    var source = req.headers['user-agent']
+    var ua = useragent.parse(source);
+    var { appleUrl, androidUrl, windowsUrl, elseUrl } = linkData
+    let redirectUrl = ''
+    if (ua.isWindows) {
+        redirectUrl = windowsUrl ?? elseUrl ?? 'https://microsoft.com'
+    } else if (isApple(ua)) {
+        redirectUrl = appleUrl ?? elseUrl ?? 'https://apple.com'
+    } else if (isAndroid(ua)) {
+        redirectUrl = androidUrl ?? elseUrl ?? 'https://google.com'
+    } else {
+        redirectUrl = elseUrl ?? windowsUrl ?? androidUrl ?? appleUrl ?? 'https://microsoft.com'
+    }
+    return redirectUrl
 }
